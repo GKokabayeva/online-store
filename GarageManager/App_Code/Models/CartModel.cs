24a600d9d78de1c81af81dec9654b1fa5a6d584f
Gaukhar <gkokabayeva@gmail.com>
2017-09-03 17:18:26 +0600

    Feature
    1. Created Models(ProductModel, ProductTypeModel, CartModel) so I can Insert, Update and Delete data in database tables(Product, ProductType, Cart).  Each classes in Models contain 3 methods for insertion, update and deletion.
    2. Created Managment UI page which allows to manage (INSERT) data to productTypes and Products

---
+++ GarageManager/App_Code/Models/CartModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for CartModel
+/// </summary>
+public class CartModel
+{
+    public string InsertCart(Cart cart)
+    {
+        try
+        {
+            GarageDBEntities db = new GarageDBEntities();
+
+            db.Carts.Add(cart);
+
+            db.SaveChanges();
+
+            return cart.DatePurchased + " was successfully inserted";
+        }
+        catch (Exception e)
+        {
+            return "Error: " + e;
+        }
+    }
+
+    public string UpdateCart(int id, Cart cart)
+    {
+        try
+        {
+            GarageDBEntities db = new GarageDBEntities();
+
+            Cart c = db.Carts.Find(id);
+
+            c.ClientId = cart.ClientId;
+            c.IsInCart = cart.IsInCart;
+            c.ProductId = cart.ProductId;
+            c.Amount = cart.Amount;
+            c.DatePurchased = cart.DatePurchased;
+
+            db.SaveChanges();
+
+            return cart.DatePurchased + " was successfully updated";
+        }
+        catch (Exception e)
+        {
+            return "Error: " + e;
+        }
+    }
+
+    public string DeleteCart(int id)
+    {
+        try
+        {
+            GarageDBEntities db = new GarageDBEntities();
+
+            Cart cart = db.Carts.Find(id);
+
+            db.Carts.Attach(cart);
+            db.Carts.Remove(cart);
+
+            db.SaveChanges();
+
+            return cart.DatePurchased + " was successfully deleted";
+        }
+        catch (Exception e)
+        {
+            return "Error: " + e;
+        }
+    }
+}
---
+++ GarageManager/App_Code/Models/ProductModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ProductModel
+/// </summary>
+public class ProductModel
+{
+    public string InsertProduct(Product product)
+    {
+        try
+        {
+            GarageDBEntities db = new GarageDBEntities();
+
+            db.Products.Add(product);
+
+            db.SaveChanges();
+
+            return product.Name + " was successfully inserted";
+        }
+        catch (Exception e)
+        {
+            return "Error: " + e;
+        }
+    }
+
+    public string UpdateProduct(int id, Product product)
+    {
+        try
+        {
+            GarageDBEntities db = new GarageDBEntities();
+
+            Product p = db.Products.Find(id);
+
+            p.Name = product.Name;
+            p.Description = product.Description;
+            p.Price = product.Price;
+            p.TypeId = product.TypeId;
+            p.Image = product.Image;
+
+            db.SaveChanges();
+
+            return product.Name + " was successfully updated";
+        }
+        catch (Exception e)
+        {
+            return "Error: " + e;
+        }
+    }
+
+    public string DeleteProduct(int id)
+    {
+        try
+        {
+            GarageDBEntities db = new GarageDBEntities();
+
+            Product product = db.Products.Find(id);
+
+            db.Products.Attach(product);
+            db.Products.Remove(product);
+
+            db.SaveChanges();
+
+            return product.Name + " was successfully deleted";
+        }
+        catch (Exception e)
+        {
+            return "Error: " + e;
+        }
+    }
+}
